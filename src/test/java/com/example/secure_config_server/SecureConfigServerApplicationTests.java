9c5d8f1f4fa3ee93bc1b80ebcc4b047d2864f178
Muhammad Mubashir <142265293+mubashir142@users.noreply.github.com>
2025-04-30 11:14:01 +0500

    Add files via upload

---
+++ src/test/java/com/example/secure_config_server/SecureConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secure_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
